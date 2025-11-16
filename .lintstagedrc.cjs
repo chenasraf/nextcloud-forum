bf59b47b2a893c61ad6d0b60e660c7b79f1d055f
Chen Asraf <casraf@pm.me>
2025-11-17 01:32:49 +0200

    build: exclude openapi from precommit formatting

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,10 @@
 module.exports = {
   '*.{ts,vue}': ['eslint --fix'],
-  '*.{scss,vue,ts,md,json}': ['prettier --write'],
+  '*.{scss,vue,ts,md}': ['prettier --write'],
+  '*.json': (files) => {
+    const filtered = files.filter(file => !file.includes('openapi.json'));
+    return filtered.length > 0 ? `prettier --write ${filtered.join(' ')}` : [];
+  },
   '*.php': [() => 'make php-cs-fixer'],
   '*Controller.php': [() => 'make openapi', () => 'git add openapi.json'],
 }

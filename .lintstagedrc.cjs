7a1853935ec0cd8dc30f4ffd1abae3a320a551bd
Chen Asraf <casraf@pm.me>
2025-11-22 23:48:42 +0200

    chore: run php tests on commit

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -5,6 +5,6 @@ module.exports = {
     const filtered = files.filter(file => !file.includes('openapi.json'));
     return filtered.length > 0 ? `prettier --write ${filtered.join(' ')}` : [];
   },
-  '*.php': [() => 'make php-cs-fixer'],
+  '*.php': [() => 'make php-cs-fixer', () => 'make test'],
   '*Controller.php': [() => 'make openapi', () => 'git add openapi.json'],
 }
